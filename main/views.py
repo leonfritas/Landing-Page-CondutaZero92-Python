14c327f507f912f155ed4b9397c0ac21ee8447eb
VasconcelosVictor <victorvasconcelos6x@hotmail.com>
2023-04-24 21:43:02 -0400

    teste

--- main/views.py
+++ main/views.py
@@ -14,10 +14,12 @@ def get_index(request):
         if form.is_valid():
             name = form.cleaned_data['name']
             email = form.cleaned_data['email']
+            birth_date = form.cleaned_data['birth_date']
 
             userGame = UserGame()
             userGame.name = name
             userGame.email = email
+            userGame.birth_date =birth_date
             userGame.save()
 
             messages.add_message(request, messages.SUCCESS, 'Dados Salvos com Sucesso!')
